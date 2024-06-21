72099e8f99bcf943e16cc83b46b2b8f381b178fb
Fernando <fernandogrfraga@gmail.com>
2024-06-21 16:55:11 -0300

    added foreing key Owner on Contatos

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -24,6 +25,11 @@ class Contato (models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True
         )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
        return f"{self.first_name} {self.last_name}"
