689622d991a454c8b54377a7658eb0b5ff73d4df
Fernando <fernandogrfraga@gmail.com>
2024-06-21 11:45:11 -0300

    added list parameters for the admin interface

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,10 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contato)
 class ContatoAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id','first_name','last_name','phone',)
+    ordering = 'id',
+    search_fields = ('first_name','id','last_name',)
+    list_per_page = 10
+    list_max_show_all = 50
+    # list_editable = 'first_name','last_name',
+
