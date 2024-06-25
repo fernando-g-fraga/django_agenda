0b587c9d3b841975d258491f4b96f0c44b88ce06
Fernando <fernandogrfraga@gmail.com>
2024-06-25 11:51:18 -0300

    add web views for data model contents.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from contact.models import Contato
 
 # Create your views here.
 
 def index(request):
+    contact = Contato.objects.all()
+
+    context = { 
+        'contact':contact
+    }
     return render(
         request,
         'contact/index.html',
+        context
     )
