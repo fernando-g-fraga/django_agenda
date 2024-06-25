096504f8e89e3a7fbfd7302f556a5bc6fbc8fe69
Fernando <fernandogrfraga@gmail.com>
2024-06-25 15:21:39 -0300

    minor adjustment on contact_views contact route

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contato
-
+from django.http import Http404
 # Create your views here.
 
 def index(request):
@@ -14,5 +14,20 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contato.objects.filter(id=contact_id).first()
+    single_contact = get_object_or_404(
+        Contato, pk=contact_id,show=True)
+
+    
+    context = { 
+        'contact':single_contact
+    }
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
