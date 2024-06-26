c5f68bf1e0a723e520cc5c2f4aedb022d3852ce7
Fernando <fernandogrfraga@gmail.com>
2024-06-26 10:50:37 -0300

    Added django dynamic forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,11 +2,29 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
 from contact.models import Contato
+from django import forms
 from django.http import Http404
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contato
+        fields = (
+            'first_name',
+            'last_name',
+            'phone')
+        
 def create(request):
-    context = { 
-
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST), 
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+    context = {
+        'form': ContactForm(), 
     }
     return render(
         request,
