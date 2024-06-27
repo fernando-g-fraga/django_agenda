3148525edbfc6e58b0d9118fc72df24954b95cb2
Fernando <fernandogrfraga@gmail.com>
2024-06-27 08:53:00 -0300

    added validation erros using clean class

--- contact/forms.py
+++ contact/forms.py
@@ -1,56 +1,61 @@
-from django.core.exceptions import ValidationError
 from django import forms
-from contact.models import Contato
-
+from django.core.exceptions import ValidationError
+from . import models
 class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'class':'classe-a classe-b',
-                'placeholder':'Digite o seu nome',
-                   }
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
         ),
         label='Primeiro Nome',
-        help_text='Texto de ajuda pra seu usuário',
+        help_text='Texto de ajuda para seu usuário',
     )
-    qualquer_coisa = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class':'classe-a classe-b',
-                'placeholder':'Digite o seu nome',
-                   }
-        ),
-        
-        help_text='Texto de ajuda pra seu usuário',
-    )
-
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init',
+        # })
     class Meta:
-        model = Contato
+        model = models.Contato
         fields = (
-            'first_name','last_name','phone'
-            )
-        
-        
-    def clean(self) -> None:
+            'first_name', 'last_name', 'phone',
+        )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
+
+    def clean(self):
         cleaned_data = self.cleaned_data
-        
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de Erro',
-                code='invalid',
-            )
-    
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de Erro2',
-                code='invalid',
-            )
-        )
+
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError('Os dois campos nao podem coincidir.')
+            self.add_error('first_name',msg)
+            self.add_error('last_name',msg)
+
 
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+           self.add_error(
+            'first_name',
+            ValidationError(
+                'add error',
+                code='invalid'
+            )
+        )
+        return first_name
+
+
