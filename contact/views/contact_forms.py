4352d3b6e754f2c5a0fc9973b592ed9f55a8c866
Fernando <fernandogrfraga@gmail.com>
2024-06-27 10:21:49 -0300

    created a new route to update the record

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from contact.models import * 
 from contact.forms import ContactForm
         
 def create(request):
+    form_action = reverse('contact:create')
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
         context = {
             'form': form,
+            'form_action': form_action,
         }
 
         if form.is_valid:
-            form.save(commit=False)
-            return redirect('contact:create')
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.id)
  
 
         return render(
@@ -18,8 +23,42 @@ def create(request):
             'contact/create.html',
             context,
         )
+        
     context = {
-        'form': ContactForm(), 
+        'form': ContactForm,
+        'form_action': form_action,
+    }
+    return render(
+        request,
+        'contact/create.html',
+        context,
+    )
+
+def update(request,contact_id):
+    contact = get_object_or_404(Contato, id=contact_id, show=True)
+    form_action = reverse('contact:update',args=(contact_id,))
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+
+        if form.is_valid:
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.id)
+ 
+
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+        
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
     }
     return render(
         request,
